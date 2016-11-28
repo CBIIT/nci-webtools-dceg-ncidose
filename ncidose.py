4720e1d124bdbd731a68caf1b1a175d547a4d8cc
kailingchen <kailingmd@yahoo.com>
2016-11-28 11:29:40 -0500

    changed Dr. Lee's email address

--- ncidose.py
+++ ncidose.py
@@ -119,12 +119,12 @@ def Send_to_recipient(email,file,date,data):
 	body = """
 	      
 	        Dear """+first+""" """+last+""",<br>
-	        <p> Thank you for your interest in the materials and software developed by the Dosimetry Unit of Radiation Epidemiology Branch at the<a href="https://dceg.cancer.gov/"> Division of Cancer Epidemiology and Genetics.</a> Attached is the STA form pre-filled with the information you entered on the web site and the materials you want to receive. Please review the STA form and email a signed copy to Dr. Choonsik Lee at leechoonsik@mail.nih.gov. You will be receiving an email with detailed download instructions once your STA form has been received and approved by Dr. Lee and NCI Technology Transfer Center.</p>
+	        <p> Thank you for your interest in the materials and software developed by the Dosimetry Unit of Radiation Epidemiology Branch at the<a href="https://dceg.cancer.gov/"> Division of Cancer Epidemiology and Genetics.</a> Attached is the STA form pre-filled with the information you entered on the web site and the materials you want to receive. Please review the STA form and email a signed copy to Dr. Choonsik Lee at choonsik.lee@nih.gov. You will be receiving an email with detailed download instructions once your STA form has been received and approved by Dr. Lee and NCI Technology Transfer Center.</p>
 	      """
 	footer = """
 	      <div>
 	        <p><i>
-	          (Note: Please do not reply to this email. If you need assistance, please contact Dr. Choonsik Lee at leechoonsik@mail.nih.gov)
+	          (Note: Please do not reply to this email. If you need assistance, please contact Dr. Choonsik Lee at choonsik.lee@nih.gov)
 	        </p></i>
 	       	<p>Sincerely,</p><p>Sent from the NCIDose Web Tool</p><p>NCIDOSEWebAdmin@mail.nih.gov</p>
 
